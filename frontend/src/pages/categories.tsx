8b30a16ead29c4b3b385bd63072681da930f127a
harisoncleytondev <harisonc081@gmail.com>
2025-12-04 17:32:37 -0300

    feat: Realizar configurações para iniciar desenvolvimento do frontend.

---
+++ frontend/src/pages/categories.tsx
@@ -0,0 +1,8 @@
+export const CategoriesPage = () => {
+  return (
+    <div className="w-screen min-h-screen bg-gradient-gblack">
+      <h1 className="bg-gradient-blue text-transparent bg-clip-text">Olá, mundo!</h1>
+      <h2 className="bg-gradient-whcyan text-transparent bg-clip-text">Teste</h2>
+    </div>
+  );
+};
