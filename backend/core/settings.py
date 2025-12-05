e6db75041b93a56cd837d875b4e2130214e84a6f
williamdev20 <williamalvs45@gmail.com>
2025-12-05 18:40:39 -0300

    feat: Configurar CORS no DRF

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,11 +45,13 @@ INSTALLED_APPS = [
     'subcategories',
     'decks',
     'cards',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -58,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'core.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
