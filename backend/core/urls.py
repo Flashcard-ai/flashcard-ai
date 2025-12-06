72a5a5d1bd80f02bd89a71886d44fcb400b8bf39
williamdev20 <williamalvs45@gmail.com>
2025-12-06 15:54:02 -0300

    feat: Adicionar cookies

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.views import LoginAPIView, RefreshTokenAPIView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("accounts/login/", LoginAPIView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", RefreshTokenAPIView.as_view(), name="token_refresh"),
     path("accounts/signup/", include("signup.urls")),
     path("categories/", include("categories.urls")),
     path("subcategories/", include("subcategories.urls")),
